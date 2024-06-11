a25d0d10349ed05c76cb0bd1f31b08597037aa14
Txxxde <158209816+Txxxde@users.noreply.github.com>
2024-06-11 02:12:52 -0300

    italia page

--- InterCult/src/app/pages/italy/italy.component.ts
+++ InterCult/src/app/pages/italy/italy.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './italy.component.css'
 })
 export class ItalyComponent {
-
+  imgcidade1: string =  'assets/roma.jpg';
+  imgcidade2: string =  'assets/roma2.jpg';
+  imgcidade3: string =  'assets/roma3.jpg';
 }
